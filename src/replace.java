b6c2578fe7f31cf3a5dc68b4f956630a1a9170c7
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-06 00:01:51 +0530

    replace

---
+++ src/replace.java
@@ -0,0 +1,7 @@
+public class replace {
+    public static void main(String args[]){
+        String s1="rehman is from parul university";
+        String replaceString=s1.replace('e','a');
+        System.out.println(replaceString);
+    }
+}
