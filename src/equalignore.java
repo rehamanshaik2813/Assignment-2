b4fa2a7f3d05aad29552c1fdfa7177fb2a446591
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-05 23:34:31 +0530

    equalignore

---
+++ src/equalignore.java
@@ -0,0 +1,11 @@
+public class equalignore {
+    public static void main(String args[]){
+        String s1="rehman";
+        String s2="rehman";
+        String s3="SHAIK";
+        String s4="rehmu";
+        System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println(s1.equalsIgnoreCase(s3));
+        System.out.println(s1.equalsIgnoreCase(s4));
+    }}
+
