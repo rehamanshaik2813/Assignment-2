3a0e47fde37c0564c1f27e60bb2fda31e039ec52
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-05 23:52:36 +0530

    isempty

---
+++ src/isempty.java
@@ -0,0 +1,9 @@
+public class isempty {
+    public static void main(String args[]){
+        String s1="";
+        String s2="rehman";
+
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+    }
+}
