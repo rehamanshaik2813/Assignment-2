fea5c40019019bd7f494fd56b38caba7e365c894
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-05 23:55:04 +0530

    join

---
+++ src/join.java
@@ -0,0 +1,9 @@
+public class join {
+    public static void main(String[] args) {
+        String date = String.join("/","05","02","2023");
+        System.out.print(date);
+        String time = String.join(":", "12","10","23");
+        System.out.println(" "+time);
+    }
+}
+
