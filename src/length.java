d9480116cbc5e8d8c29021d7813ba4edd014d769
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-05 23:59:12 +0530

    length

---
+++ src/length.java
@@ -0,0 +1,8 @@
+public class length {
+    public static void main(String args[]){
+        String s1="rehman";
+        String s2="shaik";
+        System.out.println("string length is: "+s1.length());
+        System.out.println("string length is: "+s2.length());
+    }
+}
