feebebc774f1e2735dae095d34ec981412e7bb5d
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-06 00:07:20 +0530

    startswith

---
+++ src/startswith.java
@@ -0,0 +1,9 @@
+public class startswith {
+    public static void main(String args[])
+    {
+        String s1="java string split method by javatpoint";
+        System.out.println(s1.startsWith("ja"));
+        System.out.println(s1.startsWith("java string"));
+        System.out.println(s1.startsWith("Java string"));
+    }
+}
