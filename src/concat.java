b388d60d137f8aaf6c3495ec28726f37534f3ae8
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-05 23:22:17 +0530

    concat

---
+++ src/concat.java
@@ -0,0 +1,9 @@
+public class concat {
+    public static void main(String args[]){
+        String s1="i love java";
+        s1.concat("java is a language");
+        System.out.println(s1);
+        s1=s1.concat(" is programming language");
+        System.out.println(s1);
+    }}
+
