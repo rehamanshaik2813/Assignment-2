65039ef38b11f50e0b7a6d8497787a2212c49ae0
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-06 00:04:12 +0530

    replaceall

---
+++ src/replaceall.java
@@ -0,0 +1,7 @@
+public class replaceall {
+    public static void main(String args[]){
+        String s1="my name rehman i am good boy aaa eee";
+        String replaceString=s1.replaceAll("a","e");
+        System.out.println(replaceString);
+    }
+}
