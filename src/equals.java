49c0a77e1ed6304b266289c652ea71e3e230e587
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-05 23:31:25 +0530

    equals

---
+++ src/equals.java
@@ -0,0 +1,12 @@
+public class equals {
+    public static void main(String[] args) {
+        String s1 = "javatpoint";
+        String s2 = "javatpoint";
+        String s3 = "Javatpoint";
+        System.out.println(s1.equals(s2)); // True because content is same
+        if (s1.equals(s3)) {
+            System.out.println("both strings are equal");
+        }else System.out.println("both strings are unequal");
+    }
+}
+
