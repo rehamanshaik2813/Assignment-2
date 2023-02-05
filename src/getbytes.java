d512f1cc301e57394f350c3ae80b97a4b917b0b8
SHAIK REHMAN <rehamanshaik2813@gmail.com>
2023-02-05 23:38:25 +0530

    getbytes

---
+++ src/getbytes.java
@@ -0,0 +1,12 @@
+public class getbytes {
+    public static void main(String[] args) {
+        String s1 = "ABCDEFG";
+        byte[] barr = s1.getBytes();
+        for(int i=0;i<barr.length;i++){
+            System.out.println(barr[i]);
+        }
+
+        String s2 = new String(barr);
+        System.out.println(s2);
+    }
+}
